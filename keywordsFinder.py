74d7afadc952e6bd23b456eb3a25f77c9838abc7
mxinshangshang <jiangmingxin825@live.cn>
2019-07-13 09:38:32 +0800

    Update and rename 3.py to keywordsFinder.py

--- keywordsFinder.py
+++ keywordsFinder.py
@@ -9,39 +9,72 @@ import os
 import datetime
 import time
 import ctypes
+import importlib
+importlib.reload(sys)
 
 '''
-get the win width and height
+get the win maximization
 '''
 from ctypes import windll, create_string_buffer
 user32 = ctypes.WinDLL('user32')
 SW_MAXIMISE = 3
 hWnd = user32.GetForegroundWindow()
 user32.ShowWindow(hWnd, SW_MAXIMISE)
-win_stdout = -11
-fd = windll.kernel32.GetStdHandle(win_stdout)
-cstruct = create_string_buffer(22)
-rc_struct = windll.kernel32.GetConsoleScreenBufferInfo(fd, cstruct)
-if rc_struct:
-    import struct
-    (bufx, bufy, curx, cury, wattr, left, top, right, bottom, maxx, maxy) = struct.unpack("hhhhHhhhhhh", cstruct)
-    winWidth = right - left + 1
-    winHeight = bottom - top + 1
+
+winWidth = 236
+FOREGROUND_DARKSKYBLUE = 0x03 # dark skyblue.
+FOREGROUND_DARKRED = 0x04 # dark red.
+FOREGROUND_DARKGREEN = 0x02 # dark green.
+FOREGROUND_BLUE = 0x09 # blue.
+FOREGROUND_GREEN = 0x0a # green.
+FOREGROUND_RED = 0x0c # red.
+
+STD_INPUT_HANDLE = -10
+STD_OUTPUT_HANDLE = -11
+STD_ERROR_HANDLE = -12
+
+# get handle
+std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+
+def set_cmd_text_color(color, handle=std_out_handle):
+    Bool = ctypes.windll.kernel32.SetConsoleTextAttribute(handle, color)
+    return Bool
+
+def resetColor():
+    set_cmd_text_color(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
+
+def printDarkSkyBlue(mess):
+    '''
+    dark sky blue
+    '''
+    set_cmd_text_color(FOREGROUND_DARKSKYBLUE)
+    sys.stdout.write(mess)
+    resetColor()
+
+def printRed(mess):
+    set_cmd_text_color(FOREGROUND_DARKRED)
+    sys.stdout.write(mess)
+    resetColor()
+
+def printDarkGreen(mess):
+    set_cmd_text_color(FOREGROUND_GREEN)
+    sys.stdout.write(mess)
+    resetColor()
 
 def getTime():
     '''
     return time is format of time(unit is second)
     '''
     return time.time()
- 
- 
+
+
 def getCPUClockTime():
     '''
     return time is CPU Clock time
     '''
     return time.clock()
- 
- 
+
+
 def formatTime(timevalue):
     '''
     format the time numbers
@@ -58,24 +91,7 @@ def formatTime(timevalue):
         remain = remain % 60
         #count second
         second = round(remain, 3)
-    return '%.0fh:%.0fm:%.3fs'%(hour, minute, second)
-
-def getParameters():
-    '''
-    get parameters from console command
-    '''
-    ret = []
-    if len(sys.argv) < 3 or len(sys.argv) > 4:   
-        print 'Please input correct parameter, for example:'
-        print 'No1. python search.py keyword filepath'
-        print 'No2. python search.py keyword folderpath txt'
-    else:
-        for i in range(1, len(sys.argv)):
-            #print i, sys.argv[i]
-            ret.append(sys.argv[i])
-        print winWidth * '-'
-        print '  Keyword = %s\n'%sys.argv[1]
-    return ret
+    return '%.0fh : %.0fm : %.3fs'%(hour, minute, second)
 
 
 def isFileExists(strfile):
@@ -99,7 +115,7 @@ def getTarFilePath(rootPath, fileList, dirList):
             dirList.append(dirFilePath)
             getTarFilePath(dirFilePath, fileList, dirList)
         else:
-            for z in (".tar.gz",".tar.bz2",".tar.bz",".tar.tgz",".tar",".tgz"):
+            for z in (".tar.gz",".tar.bz2",".tar.bz",".tar.tgz",".tar",".tgz",".zip",".gz"):
                 if dirFilePath.endswith(z):
                     fileList.append(dirFilePath)
 
@@ -111,24 +127,31 @@ def unRar():
     fileList = []
     dirList = []
     getTarFilePath(sourceDir, fileList, dirList)
+    print ('  Checking zip files, just wait...')
     for file in fileList:
-        print "try to unrar %s" %(file)
+        #print "try to unzip %s" %(file)
         if(isDirExists(os.path.splitext(file)[0]) == False):
             rar_command ='"C:\Program Files\WinRAR\WinRAR.exe" x %s * %s\\'%(file, os.path.splitext(file)[0])
             os.system(rar_command)
-    print ''
+    print ('  Check zip files done\n')
 
 def getFileList(strdir):
     '''
     get a type of file list in a folder
     '''
-    flist = []
+    flists = []
+
     for root, dirs, fileNames in os.walk(strdir):
         if fileNames:
             for filename in fileNames:
-                filepath = os.path.join(root, filename)
-                flist.append(filepath)
-    return flist
+                noNeedAdd = False
+                for z in (".tar.gz",".tar.bz2",".tar.bz",".tar.tgz",".tar",".tgz",".zip",".gz",".exe"):
+                    if filename.endswith(z):
+                        noNeedAdd = (noNeedAdd | True)
+                if (noNeedAdd == False):
+                    filepath = os.path.join(root, filename)
+                    flists.append(filepath)
+    return flists
 
 
 def Search(keyword, filename):
@@ -136,11 +159,11 @@ def Search(keyword, filename):
     search the keyword in a assign file
     '''
     if(isFileExists(filename) == False):
-        print 'Input filepath is wrong,please check again!'
+        print ('Input filepath is wrong,please check again!')
         sys.exit()
     linenum = 1
     findtime = 0
-    with open(filename, 'r') as fread:
+    with open(filename, 'r', encoding='UTF_8', errors ='ignore') as fread:
         lines = fread.readlines()
         for line in lines:
             rs = re.findall(keyword, line, re.IGNORECASE)
@@ -150,30 +173,31 @@ def Search(keyword, filename):
                 lsstr = line.split(keyword)
                 strlength = len(lsstr)
                 findtime = findtime + 1
-                #print strlength
                 for i in range(strlength):
                     if(i < (strlength - 1)):
-                        sys.stdout.write(lsstr[i].strip())
-                        sys.stdout.write(keyword)
+                        sys.stdout.write((lsstr[i].strip() + ' '))
+                        #sys.stdout.write(keyword)
+                        printRed(keyword)
                     else:
-                        sys.stdout.write(lsstr[i].strip() + '\n')
+                        sys.stdout.write((lsstr[i].strip() + '\n'))
             linenum = linenum + 1
     if (findtime != 0):
-       print winWidth * '-'
-       print '  Find in file: %s' %(filename)
-       print ('  keywords: %s %d times'%(keyword, findtime))
-       print winWidth * '-'
-       print ''
+       print ('-' * winWidth)
+       printDarkSkyBlue(('  Find in file: %s\n' %(filename)))
+       printDarkSkyBlue(('  Keywords: %s %d times\n'%(keyword, findtime)))
+       print ('-' * winWidth)
+       print ('')
 
 def SearchAll(keyword, strdir):
     '''
     search the keyword in a assign dir
     '''
     if(isDirExists(strdir) == False):
-        print 'Input folderpath is wrong,please check again!'
+        print ('Input folderpath is wrong,please check again!')
         sys.exit()
     unRar()
     filels = getFileList(strdir)
+    print ('  Searching...\n')
     for item in filels:
         Search(keyword, item)
 
@@ -181,18 +205,22 @@ def executeSearch():
     '''
     this is a execute search method
     '''
-    keyWords = raw_input("input keywords: ")
+    keyWords = input("  Input keywords: ")
     start = getTime()
-    sourceDir = sys.path[0]
+    sourceDir = os.path.split(os.path.realpath(__file__))[0]
     if(isDirExists(sourceDir) == False):
-        print 'Input filepath is wrong,please check again!'
+        print ('Input filepath is wrong,please check again!')
         sys.exit()
-    print 'patch: %s' %(sourceDir)
+    printDarkSkyBlue('  Current search path: %s\n' %(sourceDir))
     SearchAll(keyWords, sourceDir)
     end = getTime()
-    print '  Total cost time: %s'%formatTime(end - start)
-    print winWidth * '-'
+    printDarkGreen('  Total cost time %s\n'%formatTime(end - start))
+    print ('-' * winWidth)
+    print ('')
 
 if __name__=='__main__':
-    executeSearch()
-    os.system('pause')
+    while True:
+       try:
+           executeSearch()
+       except KeyError:
+           pass
